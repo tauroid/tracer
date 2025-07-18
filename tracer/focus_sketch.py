fe7880f647e516514667fd077e664860c4fbcb63
Adam Jacobs <adamjacobs173@googlemail.com>
2025-07-18 09:56:10 +0100

    Use tracers

--- tracer/focus_sketch.py
+++ tracer/focus_sketch.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from typing import Callable
+from typing import Callable, cast
+
+from .tracer import Tracer
 
 
 class InputParameter: ...
@@ -29,37 +31,36 @@ class OutputModel:
     parameters: list[OutputParameter]
 
 
-def transform_model(model: InputModel) -> OutputModel: ...
+transform_model = cast(Tracer[InputModel, OutputModel], ...)
 
 
 # Narrow a tracing function by supplying sub areas of `S` and `T`
-# This has no guarantee `A` and `B` live within `S` and `T`
+# This unfortunately has no guarantee `A` and `B` live within `S` and `T`
 #
 # The trick is that `a` and `b` will be called with something like
 # a mock object, which records (nested) accessed attributes
 #
 # Then `focus` can narrow down just to those
-def focus[S, T, *A, B](
-    f: Callable[[S], T], a: Callable[[S], tuple[*A]], b: Callable[[T], B]
-) -> Callable[[*A], B]: ...
+#
+# If the tracing goes outside the focussed area, that's a runtime error
+# (or `focus` time error if possible)
+def focus[S, T, A, B](
+    f: Tracer[S, T], a: Callable[[S], A], b: Callable[[T], B]
+) -> Tracer[A, B]: ...
 
 
 # Type of `transform_parameters` is inferred as
-# (list[InputParameter], datetime) -> list[OutputParameter]
+# Tracer[tuple[list[InputParameter], datetime], list[OutputParameter]]
 transform_parameters = focus(
     transform_model,
     lambda input_model: (input_model.parameters, input_model.time),
     lambda output_model: output_model.parameters,
 )
 
-# transform_parameters([x, y], datetime(2024, 5, 12))
+# transform_parameters(([x, y], datetime(2024, 5, 12)))
 # >>> [OutputParameter(...)]
 
 # transform_parameters.reverse([z])
 # >>> ([InputParameter(...), InputParameter(...)], datetime(...))
 #
 # (How can the reverse traced `time` be meaningful...)
-
-# If for some reason one of the output parameters depends on e.g. the input
-# model name (outside the focussed tracing area), that's a runtime error
-# (or `focus` time error if possible)
